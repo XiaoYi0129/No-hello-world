d0045b4f6cd5cfe965f04c2c210e1d5989685e25
XiaoYi0129 <108266314+XiaoYi0129@users.noreply.github.com>
2022-09-11 10:20:02 +0800

    Add files via upload

---
+++ E1.java
@@ -0,0 +1,17 @@
+package Lab2;
+
+public class E1{
+    public static void main(String[] args) {
+        System.out.print ("*********      ***        *          * \n");
+        System.out.print ("*       *    *     *     ***        * * \n");
+        System.out.print ("*       *   *       *   *****      *   * \n");
+        System.out.print ("*       *   *       *     *       *     * \n");
+        System.out.print ("*       *   *       *     *      *       * \n");
+        System.out.print ("*       *   *       *     *       *     * \n");
+        System.out.print ("*       *   *       *     *        *   * \n");
+        System.out.print ("*       *    *     *      *         * * \n");
+        System.out.print ("*********      ***        *          * \n");
+
+    }
+}
+
